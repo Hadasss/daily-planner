8cf7a40d8740ba97bca8caf1f845e5447102c38b
Hadasss <hadas.gadish@gmail.com>
2022-01-24 16:13:21 -0700

    general functionality - dynamically generated calendar

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,67 +3,94 @@ const container = document.querySelector(".container");
 const hourColumnUl = document.querySelector(".hour-column");
 let hour = document.querySelector(".box hour");
 const slotsColumnUl = document.querySelector(".slot-column");
-let slotItemLi = document.querySelector(".list-group-item box slot");
+let slotItemLi = document.querySelector(".slot");
 const saveBtnColumn = document.querySelector(".save-btn-column");
-let saveBtnLi = document.querySelector(".list-group-item box saveBtn");
+let saveBtnLi = document.querySelector(".saveBtn");
+const timeSlots = document.querySelectorAll(".slot");
+let textP = document.querySelector(".text-p");
+let textPId = document.querySelector(".text-p").getAttribute("id");
+let textarea = document.createElement("textarea");
 
-// const generateCalander = function () {
-//   for (let i = 0; i <= 23; i++) {
-//     // dynamically generate 24hour day
-//   }
-// };
+let slotsObj = {};
 
+// main function
 const displayCalendar = function () {
   currentDay.textContent = moment().format("MMM Do YYYY HH:MM");
-  // TODO load slots from localStorage
+  slotColorCheck();
+  loadSlots();
   setInterval(function () {
-    $(currentDay) = moment().format("DD Do YYYY");
-    // TODO color-code to indicate whether it is past, present, or future
+    slotColorCheck();
+  }, 1000 * 60 * 30);
+};
 
-let currentTime = "";
-    for (let i=0; i<slotItemLi.length; i++) {
+const slotColorCheck = function () {
+  // color-coding slots
+  timeSlots.forEach((slotItemLi) => {
+    let slotTimeId = $(slotItemLi).attr("id");
 
-      if (moment().isAfter(slotItemLi.id)) {
-        $(slotItemLi).addClass("past");
-      } else if (moment()) {
-      
-      } else {
-
-      }
-
-
-
-
-
-
+    if (moment().format("k") > parseInt(slotTimeId)) {
+      $(slotItemLi).addClass("past");
+    } else if (moment().format("k") < slotTimeId) {
+      $(slotItemLi).addClass("future");
+    } else {
+      $(slotItemLi).addClass("present");
     }
-  }, 1800000);
-slotColorCheck();
-
+  });
 };
 
-const slotColorCheck = function() {
-  const timeSlots = document.querySelectorAll(".list-group-item box slot")
-timeSlots.forEach((slotItemLi) => {
+// TODO (BUG) display night as future
+// TODO enable text change in slot on click
+$(".slot").on("click", function (event) {
+  // let targetId = event.target.id;
+  // let currentSlotLi = event.target;
+  // let currentTextarea = currentSlotLi.textContent;
+  // console.log(targetId);
+  // console.log(currentSlotLi);
 
-  let slotTimeId = $(slotItemLi).attr("id");
-  console.log(parseInt(slotTimeId));
+  // $(currentSlotLi).append(textarea);
+  // console.log(currentTextarea);
+  // $("textarea").addClass("textarea");
+  // textarea.textContent = textP.textContent;
 
-})
+  // let updatedTextarea = $(textarea).val();
+  // console.log(textarea.textContent);
+  // // BUG next line changes sets the id of first slot to event.target.id
+  // // let updatedSlotId = slotItemLi.setAttribute("id", targetId);
 
+  // // $(textPId).replaceWith($(textarea));
 
+  // saveSlotText();
 
+  let pValue = $(event.target).closest("p").text();
+  let textArea = $("<textarea>").val(pValue);
+  $(event.target).append(textArea);
 
-}
+  console.log(pValue);
+  console.log($(textArea).val());
+  $(textP).replaceWith(textArea);
+});
 
+$(".slot").on("blur", function (event) {
+  //
+});
 
-
-const changeSlot = function () {
-  // TODO enable text change in slot
-  // TODO enable save on blur after writing in slot
-  // TODO save saved slot to localStorage
+// save slotText to localStirage
+const saveSlotText = function () {
+  localStorage.setItem("slotsObj", JSON.stringify(slotsObj));
 };
 
-// TODO
+// TODO save slot to localStorage
+const loadSlots = function () {
+  slotsObj = JSON.parse(localStorage.getItem(slotsObj));
+
+  if (!slotsObj) {
+    slotsObj = {
+      hour: "", // should be slot id
+      text: "", // should be $(textarea).val()
+    };
+  }
+
+  // TODO loop over slots object to display existing slots
+};
 
 displayCalendar();
